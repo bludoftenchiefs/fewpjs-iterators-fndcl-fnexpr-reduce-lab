7f1eca7b7b63c1946fac3357b34d081a9dfd3968
rilith <bludoftenchiefs@protonmail.com>
2021-05-15 09:42:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+ 
+ const totalBatteries = [4].reduce(function(addition, element){
+        return element * 7 + addition}, 3);
+
